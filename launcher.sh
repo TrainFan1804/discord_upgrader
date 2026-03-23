1e707f4e976079a5d8a16a68230aa02d537bd2a4
o.le <oleschulz123@gmail.com>
2026-03-23 19:49:00 +0100

    Making script more robust

--- launcher.sh
+++ launcher.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-BASE_DIR="$HOME/test"
+set -euo pipefail
+
+BASE_DIR=$(cd -- "$(dirname -- "$0")" && pwd)
 APP_DIR="$BASE_DIR/app"
 TEMP_DIR="$BASE_DIR/temp"
 
@@ -11,21 +13,22 @@ mkdir -p "$APP_DIR" "$TEMP_DIR"
 # follow redirect to URL for newest version
 REMOTE_URL=$(curl -sI "$DOWNLOAD_URL" | grep -i location | awk '{print $2}' | tr -d '\r')
 EXTENSION=".tar.gz"
-REMOTE_VERSION=$(echo "$REMOTE_URL" | grep -oP '([0-9]+)\.tar\.gz'| sed -e "s/$EXTENSION$//")
+REMOTE_VERSION=$(echo "$REMOTE_URL" | grep -oP 'discord-\d\.\d\.([0-9]+)\.tar\.gz'| sed -e "s/$EXTENSION$//")
 
-TARGET_DIR="$APP_DIR/Discord_$REMOTE_VERSION"
-DOWNLOAD_ARTIFACT="$TEMP_DIR/Discord_$REMOTE_VERSION.tar.gz"
+TARGET_DIR="$APP_DIR/$REMOTE_VERSION"
+DOWNLOAD_ARTIFACT="$TEMP_DIR/$REMOTE_VERSION.tar.gz"
+
+if [ ! -f "$DOWNLOAD_ARTIFACT" ]; then
+    echo "Start dowloading Discord version $REMOTE_VERSION..."
+    curl -L --fail "$DOWNLOAD_URL" -o "$DOWNLOAD_ARTIFACT"
+else
+    echo "Found $DOWNLOAD_ARTIFACT in folder, skip download"
+fi
 
 if [ ! -d "$TARGET_DIR" ]; then
-
-    if [ ! -d "$DOWNLOAD_ARTIFACT" ]; then
-        echo "Start download artifact..."
-        curl -L "$DOWNLOAD_URL" -o "$DOWNLOAD_ARTIFACT"
-    fi
-
     mkdir -p "$TARGET_DIR"
-
-    tar -xzf "$TEMP_DIR/Discord_$REMOTE_VERSION.tar.gz" -C "$TARGET_DIR" --strip-components=1
+    echo "Start extracting"
+    tar -xzf "$TEMP_DIR/$REMOTE_VERSION.tar.gz" -C "$TARGET_DIR" --strip-components=1
 fi
 
 if [ -f "$TARGET_DIR/Discord" ]; then
